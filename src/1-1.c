740da9f750597b2eff561e8027a030450add32cc
boylerplet <amrutwali@gmail.com>
2025-05-27 09:45:41 +0530

    [01] Add question to 1-1

--- src/1-1.c
+++ src/1-1.c
@@ -1,5 +1,11 @@
+/**
+ * 1-1.c
+ * Run the ``hello, world'' program on your system. 
+ * Experiment with leaving out parts of the program,
+ * to see what error messages you get.
+ */
 #include <stdio.h>
 
-main() {
+void main() {
 	printf("hello, world\n");
 }
